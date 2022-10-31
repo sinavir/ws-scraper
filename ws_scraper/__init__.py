b766503db4653a3566a079a2462deb775606b130
sinavir <sinavir@sinavir.fr>
2022-10-31 11:28:45 +0100

    less verbose; close db between transactions

--- ws_scraper/__init__.py
+++ ws_scraper/__init__.py
@@ -10,7 +10,7 @@ import json
 from os.path import isfile as path_exists
 from time import time
 
-logging.basicConfig(level = logging.DEBUG)
+logging.basicConfig(level = logging.INFO)
 
 def python_dict_diff(a, b):
     sa, sb = set(a.items()), set(b.items())
@@ -34,23 +34,23 @@ async def init_db(db_path):
 
 async def listen(url, db_path):
     await init_db(db_path)
-    async with aiosqlite.connect(db_path) as db:
-        currentState  = None
-        async for ws in websockets.connect(url):
-            try: 
-                async for msg in ws:
-                    newState = json.loads(msg)
-                    if currentState is None:
-                        diff = newState
-                    else:
-                        diff = python_dict_diff(newState, currentState)
-                    currentState = newState
-                    if diff != {}:
+    currentState  = None
+    async for ws in websockets.connect(url):
+        try: 
+            async for msg in ws:
+                newState = json.loads(msg)
+                if currentState is None:
+                    diff = newState
+                else:
+                    diff = python_dict_diff(newState, currentState)
+                currentState = newState
+                if diff != {}:
+                    async with aiosqlite.connect(db_path) as db:
                         await db.execute('INSERT INTO data (date, json_diff) VALUES ( ? , ? )', (int(time()), json.dumps(diff)))
-                        logging.info(f"inserted data into db. diff={diff}")
-            except websockets.ConnectionClosed as e:
-                logging.info(f"Exception caught: {e}")
-            logging.info("Connection ended, retrying")
+                        logging.info(f"Inserted data into db. diff={diff}")
+        except websockets.ConnectionClosed as e:
+            logging.info(f"Exception caught: {e}")
+        logging.info("Connection ended, retrying")
     logging.info("End of script")
 
 def main():
